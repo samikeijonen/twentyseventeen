73fb3aafd0ac28bcef5e233421e9c13dd58c0379
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-10-08 22:28:59 +0300

    Remove extra whitespace.

--- inc/functions-icons.php
+++ inc/functions-icons.php
@@ -6,15 +6,15 @@
  * @subpackage Twenty_Seventeen
  * @since 1.0
  */
- 
+
 /**
  * Add SVG definitions to the footer.
  */
 function twentyseventeen_include_svg_icons() {
-	
+
 	// Define SVG sprite file.
 	$svg_icons = get_template_directory() . '/assets/images/svg-icons.svg';
-	
+
 	// If it exists, include it.
 	if ( file_exists( $svg_icons ) ) {
 		echo file_get_contents( $svg_icons );
@@ -22,7 +22,7 @@ function twentyseventeen_include_svg_icons() {
 
 }
 add_action( 'wp_footer', 'twentyseventeen_include_svg_icons', 9999 );
- 
+
 /**
  * Return SVG markup.
  *
@@ -41,12 +41,12 @@ function twentyseventeen_get_svg( $args = array() ) {
 	if ( empty( $args ) ) {
 		return esc_html__( 'Please define default parameters in the form of an array.', 'twentyseventeen' );
 	}
-	
+
 	// Define an icon.
 	if ( false === array_key_exists( 'icon', $args ) ) {
 		return esc_html__( 'Please define an SVG icon filename.', 'twentyseventeen' );
 	}
-	
+
 	// Set defaults.
 	$defaults = array(
 		'icon'        => '',
@@ -54,48 +54,48 @@ function twentyseventeen_get_svg( $args = array() ) {
 		'desc'        => '',
 		'aria_hidden' => true, // Hide from screen readers.
 	);
-	
+
 	// Parse args.
 	$args = wp_parse_args( $args, $defaults );
-	
+
 	// Set aria hidden.
 	$aria_hidden = '';
-	
+
 	if ( true === $args['aria_hidden'] ) {
 		$aria_hidden = ' aria-hidden="true"';
 	}
-	
+
 	// Set ARIA.
 	$aria_labelledby = '';
-	
+
 	if ( $args['title'] && $args['desc'] ) {
 		$aria_labelledby = ' aria-labelledby="title desc"';
 	}
-	
+
 	// Begin SVG markup.
 	$svg = '<svg class="icon icon-' . esc_html( $args['icon'] ) . '"' . $aria_hidden . $aria_labelledby . ' role="img">';
-		
+
 		// If there is a title, display it.
 		if ( $args['title'] ) {
 			$svg .= '<title>' . esc_html( $args['title'] ) . '</title>';
 		}
-		
+
 		// If there is a description, display it.
 		if ( $args['desc'] ) {
 			$svg .= '<desc>' . esc_html( $args['desc'] ) . '</desc>';
 		}
-		
+
 		// Use absolute path in the Customizer so that icons show up in there.
 		if ( is_customize_preview() ) {
 			$svg .= '<use xlink:href="' . get_template_directory_uri() . '/assets/images/svg-icons.svg' . '#icon-' . esc_html( $args['icon'] ) . '"></use>';
 		} else {
 			$svg .= '<use xlink:href="#icon-' . esc_html( $args['icon'] ) . '"></use>';
 		}
-		
+
 		$svg .= '</svg>';
-		
+
 	return $svg;
-	
+
 }
 
 /**
@@ -117,10 +117,10 @@ function twentyseventeen_do_svg( $args = array() ) {
  * @return string $item_output The menu item output with social icon.
  */
 function twentyseventeen_nav_menu_social_icons( $item_output, $item, $depth, $args ) {
-	
+
 	// Get supported social icons.
 	$social_icons = twentyseventeen_social_links_icons();
-	
+
 	// Change SVG icon inside social links menu if there is supported URL.
 	if ( 'social' == $args->theme_location ) {
 		foreach ( $social_icons as $attr => $value ) {
@@ -131,17 +131,17 @@ function twentyseventeen_nav_menu_social_icons( $item_output, $item, $depth, $ar
 	}
 
 	return $item_output;
-	
+
 }
 add_filter( 'walker_nav_menu_start_el', 'twentyseventeen_nav_menu_social_icons', 10, 4 );
-	
+
 /**
  * Returns an array of supported social links (URL and icon name).
  *
  * @return array $social_links_icons
  */
 function twentyseventeen_social_links_icons() {
-	
+
 	// Supported social links icons.
 	$social_links_icons = array(
 		'codepen.io'      => 'codepen',
@@ -176,5 +176,5 @@ function twentyseventeen_social_links_icons() {
 	);
 
 	return apply_filters( 'twentyseventeen_social_links_icons', $social_links_icons );
-	
+
 }
